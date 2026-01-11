70f8b5d1b4daa70e5d2643cb884ff5227e67a02e
yogeshramchandani <yogeshramchandani8824@gmail.com>
2026-01-11 14:38:08 +0530

    search console verfication

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import { Analytics } from "@vercel/analytics/next"
 import "./globals.css"
-
+import { GoogleAnalytics } from '@next/third-parties/google'
 // Initialize the font
 const inter = Inter({ subsets: ["latin"] })
 
@@ -51,6 +51,7 @@ export default function RootLayout({
       <body className={`${inter.className} font-sans antialiased`}>
         {children}
         <Analytics />
+        <GoogleAnalytics gaId="GTM-K5Z68C6V" />
       </body>
     </html>
   )
