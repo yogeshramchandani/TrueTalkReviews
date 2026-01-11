af58a3b2c719768381511f8b2d4b4df9ac584a65
yogeshramchandani <yogeshramchandani8824@gmail.com>
2026-01-11 14:07:03 +0530

    fixed bugs

--- app/robots.ts
+++ app/robots.ts
@@ -6,7 +6,7 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
       disallow: ['/service-provider-dashboard/',
-      '/auth',] // Don't let Google scan private dashboards
+      '/auth/',] // Don't let Google scan private dashboards
     },
     sitemap: 'https://true-talk-reviews.vercel.app/sitemap.xml',
   }
